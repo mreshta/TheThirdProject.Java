3118b1bd67b49b8d056c55ed033e85a6cdb0b3f7
mreshta <mreshta10@gmail.com>
2023-08-14 16:32:04 -0700

    This is the third git project

---
+++ src/ThirdGitPracticeProject/PracticeClass.java
@@ -0,0 +1,10 @@
+package ThirdGitPracticeProject;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is the Third Project and multiple packge and above ten classes");
+
+	}
+
+}
