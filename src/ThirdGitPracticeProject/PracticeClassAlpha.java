1290ce39c3dca8b8e85622f4cf534d1075553ec8
mreshta <mreshta10@gmail.com>
2023-08-14 17:28:45 -0700

    New

---
+++ src/ThirdGitPracticeProject/PracticeClassAlpha.java
@@ -0,0 +1,10 @@
+package ThirdGitPracticeProject;
+
+public class PracticeClassAlpha {
+
+	public static void main(String[] args) {
+		System.out.println("This is the Third Project and multiple packge and above ten classes");
+
+	}
+
+}
